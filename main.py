ea0cd7136b51c6e038487dc86f8b48f078aafc6e
BenjaminBe <benjamin.berge@ulb.be>
2021-02-26 20:29:01 +0100

    initial

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 26 17:45:01 2021
+
+@author: Bergé Benjamin, Wathelet Jolan, Anicet
+"""
+
+import csv
+
+if __name__ == "__main__":
+    db1 = to_array('test.csv')
